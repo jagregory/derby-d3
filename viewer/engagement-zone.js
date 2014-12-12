db50dd4f107d2a3d5e3ef2a54e5fcbc83e485952
James Gregory <james@jagregory.com>
2014-12-12 15:12:14 +1100

    Handle wrap around issue on bounding box

--- viewer/engagement-zone.js
+++ viewer/engagement-zone.js
@@ -10,15 +10,12 @@ function constrainedX(x, left, right) {
 
 let angleRadians = (p1, p2) => Math.atan2(p2[1] - p1[1], p2[0] - p1[0])
 let angleRadiansLine = l => angleRadians(l[0], l[1])
-let sortLines = (l1, l2) => angleRadiansLine(l1) - angleRadiansLine(l2)
-let sortLinesHorizontally = (y, l1, l2) => {
-  if (l1[0][1] < y) {
-    return l1[0][0] - l2[0][0]
-  } else {
-    console.log('inverted')
-    return l2[0][0] - l1[0][0]
-  }
+let sortByAngle = (l1, l2) => {
+  let lr = angleRadiansLine(l1),
+    rr = angleRadiansLine(l2)
+  return lr > 0 && rr > 0 || lr < 0 && rr < 0 ? lr - rr : rr - lr
 }
+let sortByX = (y, l1, l2) => l1[0][1] < y ? l1[0][0] - l2[0][0] : l2[0][0] - l1[0][0]
 
 function update() {
   let players = d3.selectAll('.player')[0]
@@ -26,16 +23,13 @@ function update() {
   let centreLine = d3.select('#centreLine'),
     left = parseInt(centreLine.attr('x1')),
     right = parseInt(centreLine.attr('x2')),
-    y = parseInt(centreLine.attr('y1')),
-    centre = left+(right-left)/2
+    y = parseInt(centreLine.attr('y1'))
 
   let ezlines = players
     .map(pos => [[pos[0], pos[1]], [constrainedX(pos[0], left, right), y]])
-    .sort((l1, l2) => sortLines(l1, l2) || sortLinesHorizontally(y, l1, l2))
+    .sort((l1, l2) => sortByAngle(l1, l2) || sortByX(y, l1, l2))
   let front = ezlines[0],
     back = ezlines[ezlines.length-1]
-
-  console.log('Front:', angleRadiansLine(front), 'Back:', angleRadiansLine(back))
 
   d3.select('svg g')
     .selectAll('line.ezline')
