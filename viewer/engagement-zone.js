d5a05f5930fa77ed3f8681b6138ce343421d2e1e
James Gregory <james@jagregory.com>
2014-12-16 08:33:44 +1100

    Show intersection on both sides of the track

--- viewer/engagement-zone.js
+++ viewer/engagement-zone.js
@@ -55,15 +55,24 @@ function update() {
   let centreLine = d3.select('#centreLine'),
     left = parseInt(centreLine.attr('x1')),
     right = parseInt(centreLine.attr('x2')),
-    y = parseInt(centreLine.attr('y1'))
+    cy = parseInt(centreLine.attr('y1'))
 
   let ezlines = players
-    .map(pos => [[pos[0], pos[1]], [constrainedX(pos[0], left, right), y]])
-    .sort((l1, l2) => sortByAngle(l1, l2) || sortByX(y, l1, l2))
+    .map(pos => {
+      let x1 = pos[0],
+        y1 = pos[1],
+        x2 = constrainedX(x1, left, right),
+        y2 = cy,
+        dx = x2 - x1,
+        dy = y2 - y1
+      return [[x1 - dx, y1 - dy], [x2, y2]]
+    })
+    .sort((l1, l2) => sortByAngle(l1, l2) || sortByX(cy, l1, l2))
   let front = ezlines[0],
     back = ezlines[ezlines.length-1]
 
   let inside = Snap._.wrap(document.getElementById('inside'));
+  let outside = Snap._.wrap(document.getElementById('outside'));
   let frontLine = Snap._.wrap(createSvgElement('line', {
     d: 'M' + front[0][0] + ' ' + front[0][1] + ' L' + front[1][0] + ' ' + front[1][1]
   }))
@@ -91,16 +100,18 @@ function update() {
     .enter().append('line')
       .attr('class', 'ezline')
 
-  let frontIntersection = getIntersections(inside, frontLine)
-  let backIntersection = getIntersections(inside, backLine)
-  if (frontIntersection) {
+  let frontInsideIntersection = getIntersections(inside, frontLine),
+    frontOutsideIntersection = getIntersections(outside, frontLine),
+    backInsideIntersection = getIntersections(inside, backLine),
+    backOutsideIntersection = getIntersections(outside, backLine)
+  if (frontInsideIntersection) {
     d3.select('#track')
       .selectAll('#cr')
-      .data([frontIntersection, backIntersection])
+      .data([frontInsideIntersection, frontOutsideIntersection, backInsideIntersection, backOutsideIntersection])
       .attr('r', 3)
       .attr('fill', 'red')
-      .attr('cx', d => d.x)
-      .attr('cy', d => d.y)
+      .attr('cx', d => d ? d.x : 0) // TODO: Handle out-of-bounds checks, extend the line outwards a bit
+      .attr('cy', d => d ? d.y : 0)
       .enter().append('circle').attr('id', 'cr')
     // frontLine.setAttribute('d', 'M' + front[0][0] + ' ' + front[0][1] + ' L' + intersection.x + ' ' + intersection.y)
   }
